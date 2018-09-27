0f7f73f645c95e3fa82bcd8e5195ea01b4e38808
gennabarge <gennabarge@10.0.0.5>
2018-09-27 08:40:22 -0400

    Final code, added if statements, while loop

--- src/U1L03RandomGuessingGame.java
+++ src/U1L03RandomGuessingGame.java
@@ -7,12 +7,13 @@ public class U1L03RandomGuessingGame {
 		// TODO Auto-generated method stub
 //first allow the computer to obtain a random number 
 		int computerNum = (int)(Math.random() * 100) + 1;
+		int count = 0; 
 
 		// ^ (see the slide for the code
 //then, show a display screen and instructions, may input -1 etc. 
 		JOptionPane.showMessageDialog(null, "Welcome to the number guesser!" +
 "\nInput any number, and if you choose to quit at any point, enter -1." +
-				"/nLet's get started!"); 
+				" Let's get started!"); 
 
 
 // ask for user input as to what the number may be
@@ -23,15 +24,26 @@ public class U1L03RandomGuessingGame {
 		while (guess != -1) {
 			if (guess > computerNum) {
 				JOptionPane.showMessageDialog(null, "Looks like your guess is a little too high!");
+				count++;
+				input = JOptionPane.showInputDialog("Guess again!");
+			    guess = Integer.parseInt(input);
 			}
 			if (guess < computerNum) {
-				
+				JOptionPane.showMessageDialog(null, "A little too low!");
+				count++; 
+				input = JOptionPane.showInputDialog("Guess again!");
+				guess = Integer.parseInt(input);
 			}
 			if (guess == computerNum) { 
+				JOptionPane.showMessageDialog(null, "Congrats!! You guessed it correctly!");
+				JOptionPane.showMessageDialog(null, "it took you " + count + " guesses");
 				
+				break; 
 			}
 			if (guess == -1) {
-				
+				JOptionPane.showMessageDialog(null, "Thanks for playing, bye!");
+				JOptionPane.showMessageDialog(null, "you attempted this game " + count + " times");
+				break; 
 			}
 		}
 		
